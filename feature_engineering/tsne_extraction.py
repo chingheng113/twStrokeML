7697c1ca503daf3d167f5b61f7fc350dc920610a
aalinc9 <chingheng113@gmail.com>
2018-09-03 11:46:44 -0400

    down t-sne feature selection to 2D

--- feature_engineering/tsne_extraction.py
+++ feature_engineering/tsne_extraction.py
@@ -7,7 +7,7 @@ from sklearn.feature_selection import SelectFromModel
 
 
 def tsne_features_add(x_data, seed):
-    t_sne = TSNE(n_components=3, perplexity=30, learning_rate=200.0, random_state=seed, verbose=1)
+    t_sne = TSNE(n_components=2, perplexity=30, learning_rate=200.0, random_state=seed, verbose=1)
     new_features = t_sne.fit_transform(x_data)
     new_x_data = np.concatenate([x_data, new_features], axis=1)
     return new_x_data

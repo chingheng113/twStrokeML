998390b684ae912aab9fa034a3fd51e61bc7f11a
aalinc9 <chingheng113@gmail.com>
2018-09-05 13:53:33 -0400

    new feature selection

--- feature_engineering/rf_selection.py
+++ feature_engineering/rf_selection.py
@@ -1,6 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, train_test_split
 import numpy as np
 import pandas as pd
 from my_utils import data_util, plot_fig, performance_util
@@ -15,7 +15,7 @@ def plot_all_features(importances, feature_names):
     plt.figure(1)
     plt.title('Feature Importances')
     plt.barh(range(len(indices)), importances[indices], color='b', align='center')
-    plt.yticks(range(len(indices)), feature_names[indices])
+    plt.yticks(range(len(indices)), feature_names[indices], fontsize=4)
     plt.xlabel('Relative Importance')
     plt.show()
 
@@ -24,9 +24,7 @@ if __name__ == '__main__':
     # https://chrisalbon.com/machine_learning/trees_and_forests/feature_selection_using_random_forest/
     # wholeset_Jim_nomissing_validated.csv
     seed = 7
-    n_fold = 2
     np.random.seed(seed)
-    kfold = StratifiedKFold(n_splits=n_fold, shuffle=True, random_state=seed)
     id_data, x_data, y_data = data_util.get_poor_god('wholeset_Jim_nomissing_validated.csv')
     feature_names = x_data.columns.values
 
@@ -34,19 +32,19 @@ if __name__ == '__main__':
     # features that have an importance of more than XX
     rf = RandomForestClassifier(n_estimators=100, criterion='entropy', random_state=seed)
     sfm = SelectFromModel(rf, threshold=5e-3)
-    for index, (train, test) in enumerate(kfold.split(x_data, y_data)):
-        rf.fit(data_util.scale(x_data.iloc[train]), y_data.iloc[train])
-        importances = rf.feature_importances_
-        # plot_all_features(importances, feature_names)
-        # Train the selector
-        sfm.fit(x_data.iloc[train], y_data.iloc[train])
-        # Print the names of the most important features
-        for feature_list_index in sfm.get_support(indices=True):
-            print(feature_names[feature_list_index])
-        # Transform the data to create a new dataset containing only the most important features
-        # X_important_train = x_data.iloc[train][x_data.iloc[train].columns[sfm.get_support(indices=True)]]
-        # X_important_train = sfm.transform(x_data.iloc[train])
-        print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    x_data, x_hold, y_data, y_hold = train_test_split(x_data, y_data, test_size=0.3, random_state=seed)
+    rf.fit(data_util.scale(x_data), y_data)
+    importances = rf.feature_importances_
+    plot_all_features(importances, feature_names)
+    # Train the selector
+    sfm.fit(x_data, y_data)
+    # Print the names of the most important features
+    for feature_list_index in sfm.get_support(indices=True):
+        print(feature_names[feature_list_index])
+    # Transform the data to create a new dataset containing only the most important features
+    # X_important_train = x_data.iloc[train][x_data.iloc[train].columns[sfm.get_support(indices=True)]]
+    # X_important_train = sfm.transform(x_data.iloc[train])
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 
 
     id_data_all, x_data_all, y_data_all = data_util.get_individual('wholeset_Jim_nomissing_validated.csv')

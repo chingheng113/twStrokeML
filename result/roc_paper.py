9dc0fa84a9ed7a0cdafa6ec21a9ff6ddaed8e15b
aalinc9 <chingheng113@gmail.com>
2019-02-06 08:59:48 -0500

    back up

--- result/roc_paper.py
+++ result/roc_paper.py
@@ -12,46 +12,100 @@ if __name__ == '__main__':
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(20,20))
     plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=0.3)
 
+    # --- training
+    # plt.subplot(221)
+    # for inx, model_name in enumerate(model_names):
+    #     mean_fpr, mean_tpr, mean_auc, std_auc = performance_util.average_roc_auc(model_name, 'all', 'ischemic', 'hold')
+    #     plt.plot(mean_fpr, mean_tpr,
+    #              label=present_names[inx]+' (AUC = %0.3f ± %0.3f)' % (mean_auc, std_auc),
+    #              lw=1, alpha=.8)
+    # plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='black', alpha=.8)
+    # plt.title('Ischemic stroke with all features', fontsize=14)
+    # plt.legend(loc="lower right", prop={'size': 12})
+    #
+    # plt.subplot(222)
+    # for inx, model_name in enumerate(model_names):
+    #     mean_fpr, mean_tpr, mean_auc, std_auc = performance_util.average_roc_auc(model_name, 'all', 'hemorrhagic', 'hold')
+    #     plt.plot(mean_fpr, mean_tpr,
+    #              label=present_names[inx]+' (AUC = %0.3f ± %0.3f)' % (mean_auc, std_auc),
+    #              lw=1, alpha=.8)
+    # plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='black', alpha=.8)
+    # plt.title('Hemorrhagic stroke with all features', fontsize=14)
+    # plt.legend(loc="lower right", prop={'size': 12})
+    #
+    # plt.subplot(223)
+    # for inx, model_name in enumerate(model_names):
+    #     mean_fpr, mean_tpr, mean_auc, std_auc = performance_util.average_roc_auc(model_name, 'fs', 'ischemic', 'hold')
+    #     plt.plot(mean_fpr, mean_tpr,
+    #              label=present_names[inx]+' (AUC = %0.3f ± %0.3f)' % (mean_auc, std_auc),
+    #              lw=1, alpha=.8)
+    # plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='black', alpha=.8)
+    # plt.title('Ischemic stroke with selected features', fontsize=14)
+    # plt.legend(loc="lower right", prop={'size': 12})
+    #
+    # plt.subplot(224)
+    # for inx, model_name in enumerate(model_names):
+    #     mean_fpr, mean_tpr, mean_auc, std_auc = performance_util.average_roc_auc(model_name, 'fs', 'hemorrhagic', 'hold')
+    #     plt.plot(mean_fpr, mean_tpr,
+    #              label=present_names[inx]+' (AUC = %0.3f ± %0.3f)' % (mean_auc, std_auc),
+    #              lw=1, alpha=.8)
+    # plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='black', alpha=.8)
+    # plt.title('Hemorrhagic stroke with selected features', fontsize=14)
+    # plt.legend(loc="lower right", prop={'size': 12})
+
+    # --- testing
     plt.subplot(221)
+    for inx, model_name in enumerate(model_names):
+        mean_fpr, mean_tpr, mean_auc, std_auc = performance_util.average_roc_auc(model_name, 'all', 'ischemic', 'test')
+        plt.plot(mean_fpr, mean_tpr,
+                 label=present_names[inx]+' (AUC = %0.3f ± %0.3f)' % (mean_auc, std_auc),
+                 lw=1, alpha=.8)
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='black', alpha=.8)
+    plt.title('Ischemic stroke with all features', fontsize=14)
+    plt.legend(loc="lower right", prop={'size': 12})
+
+    plt.subplot(222)
+    for inx, model_name in enumerate(model_names):
+        mean_fpr, mean_tpr, mean_auc, std_auc = performance_util.average_roc_auc(model_name, 'all', 'hemorrhagic', 'test')
+        plt.plot(mean_fpr, mean_tpr,
+                 label=present_names[inx]+' (AUC = %0.3f ± %0.3f)' % (mean_auc, std_auc),
+                 lw=1, alpha=.8)
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='black', alpha=.8)
+    plt.title('Hemorrhagic stroke with all features', fontsize=14)
+    plt.legend(loc="lower right", prop={'size': 12})
+
+    plt.subplot(223)
     for inx, model_name in enumerate(model_names):
         mean_fpr, mean_tpr, mean_auc, std_auc = performance_util.average_roc_auc(model_name, 'fs', 'ischemic', 'test')
         plt.plot(mean_fpr, mean_tpr,
                  label=present_names[inx]+' (AUC = %0.3f ± %0.3f)' % (mean_auc, std_auc),
                  lw=1, alpha=.8)
     plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='black', alpha=.8)
-    plt.title('ischemic - traing - all', fontsize=14)
+    plt.title('Ischemic stroke with selected features', fontsize=14)
     plt.legend(loc="lower right", prop={'size': 12})
 
-    plt.subplot(222)
-    for inx, model_name in enumerate(model_names):
-        mean_fpr, mean_tpr, mean_auc, std_auc = performance_util.average_roc_auc(model_name, 'fs', 'ischemic', 'hold')
-        plt.plot(mean_fpr, mean_tpr,
-                 label=present_names[inx]+' (AUC = %0.3f ± %0.3f)' % (mean_auc, std_auc),
-                 lw=1, alpha=.8)
-    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='black', alpha=.8)
-    plt.title('ischemic - hold - fs', fontsize=14)
-    plt.legend(loc="lower right", prop={'size': 12})
-
-    plt.subplot(223)
+    plt.subplot(224)
     for inx, model_name in enumerate(model_names):
         mean_fpr, mean_tpr, mean_auc, std_auc = performance_util.average_roc_auc(model_name, 'fs', 'hemorrhagic', 'test')
         plt.plot(mean_fpr, mean_tpr,
                  label=present_names[inx]+' (AUC = %0.3f ± %0.3f)' % (mean_auc, std_auc),
                  lw=1, alpha=.8)
     plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='black', alpha=.8)
-    plt.title('hemorrhagic - traing - all', fontsize=14)
-    plt.legend(loc="lower right", prop={'size': 12})
-
-    plt.subplot(224)
-    for inx, model_name in enumerate(model_names):
-        mean_fpr, mean_tpr, mean_auc, std_auc = performance_util.average_roc_auc(model_name, 'fs', 'hemorrhagic', 'hold')
-        plt.plot(mean_fpr, mean_tpr,
-                 label=present_names[inx]+' (AUC = %0.3f ± %0.3f)' % (mean_auc, std_auc),
-                 lw=1, alpha=.8)
-    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='black', alpha=.8)
-    plt.title('hemorrhagic - hold - all', fontsize=14)
+    plt.title('Hemorrhagic stroke with selected features', fontsize=14)
     plt.legend(loc="lower right", prop={'size': 12})
 
     fig.savefig("roc.png", dpi=300)
     plt.show()
     print('done')
+
+
+
+# plt.subplot(222)
+# for inx, model_name in enumerate(model_names):
+#     mean_fpr, mean_tpr, mean_auc, std_auc = performance_util.average_roc_auc(model_name, 'fs', 'ischemic', 'hold')
+#     plt.plot(mean_fpr, mean_tpr,
+#              label=present_names[inx]+' (AUC = %0.3f ± %0.3f)' % (mean_auc, std_auc),
+#              lw=1, alpha=.8)
+# plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='black', alpha=.8)
+# plt.title('ischemic - hold - fs', fontsize=14)
+# plt.legend(loc="lower right", prop={'size': 12})

85bc64b65281483b9561c16e3cde489b9f55dcd7
chingheng113 <chingheng113@gmail.com>
2019-06-30 17:35:20 -0400

    back up

---
+++ try/single_mrs1.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt   #Data visualisation libraries
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from my_utils import data_util, performance_util
+from sklearn.metrics import roc_auc_score
+
+
+id_train_all, x_train_all, y_train_all = data_util.get_poor_god_downsample(
+            'training_he_9.csv', sub_class='hemorrhagic')
+id_hold, x_hold, y_hold = data_util.get_poor_god(
+            'hold_he_9.csv', sub_class='hemorrhagic')
+lm = LogisticRegression()
+x = x_train_all[['MRS_1']]
+y = y_train_all
+lm.fit(x, y)
+test_x = x_hold[['MRS_1']]
+predictions = lm.predict(test_x)
+logit_roc_auc = roc_auc_score(y_hold, lm.predict(test_x))
+print(logit_roc_auc)

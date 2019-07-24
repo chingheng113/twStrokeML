1d36c50aeaed820115cc60aebcd5ed6cffb30157
Ching-Heng <chingheng113@gmail.com>
2019-07-24 15:20:17 -0400

    back up

--- try/single_mrs1.py
+++ try/single_mrs1.py
@@ -13,10 +13,10 @@ id_train_all, x_train_all, y_train_all = data_util.get_poor_god_downsample(
 id_hold, x_hold, y_hold = data_util.get_poor_god(
             'hold_is_9.csv', sub_class='ischemic')
 lm = LogisticRegression()
-x = x_train_all[['MRS_1']]
+x = x_train_all[['MRS_TX_1']]
 y = y_train_all
 lm.fit(x, y)
-test_x = x_hold[['MRS_1']]
+test_x = x_hold[['MRS_TX_1']]
 predictions = lm.predict(test_x)
 logit_roc_auc = roc_auc_score(y_hold, lm.predict(test_x))
 print(logit_roc_auc)
